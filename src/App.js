efc14a93b86271c39a25bc3c717d740df9afbb27
ks46 <konastaf46@gmail.com>
2023-08-18 12:58:41 +0300

    handleSubmit, handleDelete, handleUpdate functionality

--- src/App.js
+++ src/App.js
@@ -8,17 +8,21 @@ function App() {
     ])
 
     const handleSubmit = (event) => {
-        event.preventDefault()
-        console.log(event.target[0].value)
         // TODO Add a new task to the tasks array
         // HINT: Spread the current tasks array into a new array, add this new task on there
         // then update the state of tasks
+        event.preventDefault()
+        const task = event.target[0].value
+        const newTasks = [...tasks, {text: task, checked: false}]
+        setTasks(newTasks)
     }
 
     const handleDelete = (index) => {
         // TODO Using the provided index, remove the task from the array and update
         // state to re-render the component
         // HINT: .filter()
+        const newTasks = tasks.filter((todoItem, todoIndex) => todoIndex !== index)
+        setTasks(newTasks)
     }
 
     const handleUpdate = (index, checked) => {
@@ -26,6 +30,12 @@ function App() {
         // Change the checked property on the task
         // Update the state array to re-render the component
         // HINT: .map() or access by index
+        const newTasks = tasks.map((todoItem, todoIndex) => {
+            if (todoIndex === index)
+                return {...todoItem, checked:checked}
+            return todoItem
+        })
+        setTasks(newTasks)
     }
 
   return (
